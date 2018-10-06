760b00f240ddf8a20867a5f73e7165e8dfba5304
Rohan Jhunjhunwala <rjhunjhunwala80@berkeley.edu>
2018-10-06 05:00:13 -0700

    code

--- web_crypto.py
+++ web_crypto.py
@@ -1,3 +1,4 @@
+import rsa
 from rsa import *
 import urllib.request
 import base64
@@ -15,7 +16,8 @@ def is_valid_text(str):
     try:
         rsa.verify(str.splitlines()[0].encode("utf-8"), eval(str.splitlines()[1]), get_public_key())
         return True
-    except:
+    except Exception as e:
+        print(e)
         return False
 def get_signed_text(st):
     return st+'\n' + str((rsa.sign_hash(rsa.compute_hash(st.encode("utf-8"),"SHA-256"),get_private_key(),"SHA-256")))
