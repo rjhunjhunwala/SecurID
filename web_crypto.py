503f6afc4987ef6df00e4ea872f6c621aac3a658
Rohan Jhunjhunwala <rjhunjhunwala80@berkeley.edu>
2018-10-06 03:53:50 -0700

    code

---
+++ web_crypto.py
@@ -0,0 +1,30 @@
+import sys
+import hashlib
+from rsa import *
+import rsa
+import base64
+from flask import Flask
+
+app = Flask(__name__)
+
+def get_private_key():
+    return eval(open("private.txt", 'r').read())
+
+def get_public_key():
+    return eval(open("public.txt",'r').read())
+
+def is_valid_text(str):
+    try:
+        rsa.verify(str.splitlines()[0].encode("utf-8"), eval(str.splitlines()[1]), get_public_key())
+        return True
+    except:
+        return False
+def get_signed_text(st):
+    return st+'\n' + str((rsa.sign_hash(rsa.compute_hash(st.encode("utf-8"),"SHA-256"),get_private_key(),"SHA-256")))
+    # return st+'\n' + str(base64.b64encode(rsa.sign_hash(rsa.compute_hash(st.encode("utf-8"),"SHA-256"),get_private_key(),"SHA-256")))
+
+import urllib.request
+@app.route('/<id>')
+def get_output(id):
+    # show the user profile for that user
+    return str(is_valid_text(urllib.request.urlopen("https://raw.githubusercontent.com/rjhunjhunwala/SecurID/master/"+id).read().decode("utf-8")))
