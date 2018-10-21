0dd6512b1e65b018c231a7868acdbe1df1c95112
Rohan Jhunjhunwala <rjhunjhunwala80@berkeley.edu>
2018-10-21 13:33:07 -0700

    code

--- new_server.py
+++ new_server.py
@@ -1,7 +1,9 @@
 #!flask/bin/python
 from flask import Flask, jsonify
+from flask_restful import Resource, Api
 from random import randint
 app = Flask(__name__)
+api = Api(app)
 
 def get_joke():
     jokes = open("jokes.txt", "r")
@@ -20,9 +22,14 @@ task = {
 }
 
 
-@app.route('/')
+class Sim(Resource):
+    def get(self):
+        return start()
+
 def start():
     return jsonify(task)
+
+api.add_resource(Sim)
 
 if __name__ == '__main__':
     app.run(debug=True)
