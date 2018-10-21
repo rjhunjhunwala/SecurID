8bd143c576695735f2f4e0c438f6a712c7e4c200
Rohan Jhunjhunwala <rjhunjhunwala80@berkeley.edu>
2018-10-21 13:20:05 -0700

    code

--- new_server.py
+++ new_server.py
@@ -20,9 +20,9 @@ task = {
 }
 
 
-@app.route('/todo/api/v1.0/tasks', methods=['GET'])
+@app.route('/', methods=['GET'])
 def get_tasks():
-    return jsonify({'tasks': tasks})
+    return jsonify({'tasks': task})
 
 if __name__ == '__main__':
     app.run(debug=True)
